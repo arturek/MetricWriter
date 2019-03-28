a8da253a65c2f95ef85a52ec83a65b2b18cd5670
Artur Zgodzinski <azgodzinski@eshopworld.com>
2019-03-28 12:14:32 +0000

    Redesigned MetricWriter by separating tracked value from metric dimensions.

--- MetricWriter/Program.cs
+++ MetricWriter/Program.cs
@@ -12,7 +12,7 @@ namespace MetricWriter
     {
         static void Main(string[] args)
         {
-            if(args.Length != 1)
+            if (args.Length != 1)
             {
                 Console.WriteLine("InstrumentationKey missing");
                 return;
@@ -33,110 +33,111 @@ namespace MetricWriter
             //}
 
 
-            var metricWriter = telemetryClient.GetMetricWriter<MyMetric>("art");
-
+            var metricWriter = telemetryClient.GetMetricWriter("art", new MyMetricDimentions
+            {
+                Dim1 = "aa",
+            });
             for (int i = 0; i < 5; i++)
             {
-                metricWriter.Track(new MyMetric
-                {
-                    Value = i,
-                    Dim1 = "aa",
-                });
+                metricWriter.TrackValue(i);
 
                 Thread.Sleep(TimeSpan.FromMinutes(1));
             }
         }
     }
 
-    public class MyMetric
+    public class MyMetricDimentions
     {
-        public double Value { get; set; }
-
         public string Dim1 { get; set; }
     }
 
     public static class MetricExtensions
     {
-        public static MetricWriter<T> GetMetricWriter<T>(this TelemetryClient telemetryClient, string metricId)
+        public static MetricWriter GetMetricWriter(this TelemetryClient telemetryClient, string metricId)
         {
-            // TODO: validate parameters
+            var metric = telemetryClient.GetMetric(metricId);
+            return new MetricWriter(metric);
+        }
 
+        public static MetricWriter<T> GetMetricWriter<T>(this TelemetryClient telemetryClient, string metricId, T dimensions)
+        {
             var properties = typeof(T).GetProperties();
-            // TODO: take care of special properties
-            PropertyInfo valueProperty = null;
-            List<PropertyInfo> dimensionProperties = new List<PropertyInfo>();
-            foreach (var property in properties)
+            if (properties.Any(p => p.PropertyType != typeof(string)))
             {
-                if (property.PropertyType == typeof(double))
-                {
-                    if (valueProperty != null)
-                        throw new Exception("Multiple value properties found.");
-                    valueProperty = property;
-                }
-                else if (property.PropertyType == typeof(string))
-                {
-                    dimensionProperties.Add(property);
-                }
-                else
-                {
-                    throw new Exception("Only value or dimension properties are allowee.");
-                }
+                throw new Exception("Only value or dimension properties are allowee.");
             }
 
-            if (valueProperty == null)
-                throw new Exception("Value property is missing.");
-            if (dimensionProperties.Count > 4)
-                throw new Exception("Too many dimention specified.");
-
-            // TODO: use MetricIdentifier to support more dimensions
             var getMetricCall = typeof(TelemetryClient).GetMethod("GetMetric",
-                Enumerable.Repeat(typeof(string), dimensionProperties.Count + 1).ToArray());
+                Enumerable.Repeat(typeof(string), properties.Length + 1).ToArray());
 
             var getMetricCallParameters = new List<object>
             {
                 metricId,
             };
-            getMetricCallParameters.AddRange(dimensionProperties.Select(x => x.Name));
+            getMetricCallParameters.AddRange(properties.Select(x => x.Name));
 
-            var trackValueCall = BuildTrackValue<T>(valueProperty, dimensionProperties);
             var metric = (Metric)getMetricCall.Invoke(telemetryClient, getMetricCallParameters.ToArray());
 
-            return new MetricWriter<T>(trackValueCall, metric);
+            return new MetricWriter<T>(BuildTrackValue<T>(properties), metric);
         }
 
-        private static Func<Metric, T, bool> BuildTrackValue<T>(PropertyInfo value, List<PropertyInfo> dimensions)
+        private static Func<Metric, double, T, bool> BuildTrackValue<T>(PropertyInfo[] dimensions)
         {
             var metricParameter = Expression.Parameter(typeof(Metric), "metric");
-            var valueParameter = Expression.Parameter(typeof(T), "trackedValue");
-            var getValue = Expression.Property(valueParameter, value);
+            var dimensionsParameter = Expression.Parameter(typeof(T), "dimensions");
+            var valueParameter = Expression.Parameter(typeof(double), "trackedValue");
+
             var trackValueParameterTypes = new List<Type> { typeof(double) };
-            trackValueParameterTypes.AddRange(Enumerable.Repeat(typeof(string), dimensions.Count));
-            var trackValueParameters = new List<Expression> { getValue };
+            trackValueParameterTypes.AddRange(Enumerable.Repeat(typeof(string), dimensions.Length));
+
+            var trackValueParameters = new List<Expression> { valueParameter };
             trackValueParameters.AddRange(
-                dimensions.Select(p => Expression.Property(valueParameter, p)));
+                dimensions.Select(p => Expression.Property(dimensionsParameter, p)));
             var trackValueCall = Expression.Call(
                 metricParameter,
                 typeof(Metric).GetMethod("TrackValue", trackValueParameterTypes.ToArray()),
                 trackValueParameters.ToArray());
-            var del = Expression.Lambda(trackValueCall, metricParameter, valueParameter);
-            return (Func<Metric, T, bool>)del.Compile();
+            var del = Expression.Lambda(trackValueCall, metricParameter, valueParameter, dimensionsParameter);
+            return (Func<Metric, double, T, bool>)del.Compile();
         }
     }
 
     public class MetricWriter<T>
     {
-        private readonly Func<Metric, T, bool> _trackValue;
+        private readonly Func<Metric, double, T, bool> _metricWriter;
         private readonly Metric _metric;
 
-        internal MetricWriter(Func<Metric, T, bool> trackValue, Metric metric)
+        public MetricWriter(Func<Metric, double, T, bool> metricWriter, Metric metric)
         {
-            _trackValue = trackValue;
+            _metric = metric;
+            _metricWriter = metricWriter;
+        }
+
+        public T DefaultDimensions { get; set; }
+
+        public bool TrackValue(double value)
+        {
+            return _metricWriter(_metric, value, DefaultDimensions);
+        }
+
+        public bool TrackValue(int value, T dimensions)
+        {
+            return _metricWriter(_metric, value, dimensions);
+        }
+    }
+
+    public class MetricWriter
+    {
+        private readonly Metric _metric;
+
+        public MetricWriter(Metric metric)
+        {
             _metric = metric;
         }
 
-        public bool Track(T value)
+        public void TrackValue(double value)
         {
-            return _trackValue(_metric, value);
+            _metric.TrackValue(value);
         }
     }
 }
